243d5ea6fc0ab74910cfcabd49b2b41b63e38abb
MysticalOS <mysticalosx@gmail.com>
2026-02-24 01:28:23 -0500

    while at it, go ahead and upgrade the repent warning for far more clarity since people are missing the warning before

--- DBM-Raids-BC/Karazhan/MaidenOfVirtue.lua
+++ DBM-Raids-BC/Karazhan/MaidenOfVirtue.lua
@@ -18,33 +18,24 @@ mod:RegisterEventsInCombat(
 --TODO, rependance timer is consistent but there is an unknown trigger that happens once per kill where the timer resets?
 --Maybe reaching a health threshold resets the CD?
 --ability.id = 29511 and type = "begincast"
-local warningRepentance		= mod:NewSpellAnnounce(29511, 4)
 local warningHolyFire		= mod:NewTargetNoFilterAnnounce(29522, 2)
 
---local specWarnHolyFire		= mod:NewSpecialWarningMoveAway(29522, nil, nil, nil, 1, 2)
+local specWarnRepentance	= mod:NewSpecialWarningMoveTo(29522, nil, nil, nil, 1, 15)
 
 local timerRepentanceCD		= mod:NewVarTimer("v28-64.7", 29511, nil, nil, nil, 6)
 local timerHolyFire			= mod:NewTargetTimer(12, 29522, nil, nil, nil, 5, nil, DBM_COMMON_L.MAGIC_ICON)
 
-mod:AddRangeFrameOption(10, 29522)
+local holyGround = DBM:GetSpellName(29512)
 
 function mod:OnCombatStart(delay)
 	timerRepentanceCD:Start()
-	if self.Options.RangeFrame then
-		DBM.RangeCheck:Show(10)
-	end
 	DBM:AddMsg("Repentance has a 28-64.7 second variance. Timer has been updated to reflect this.")
-end
-
-function mod:OnCombatEnd()
-	if self.Options.RangeFrame then
-		DBM.RangeCheck:Hide()
-	end
 end
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 29511 then
-		warningRepentance:Show()
+		specWarnRepentance:Show(holyGround)
+		specWarnRepentance:Play("movetopool")
 		timerRepentanceCD:Start()
 	end
 end
